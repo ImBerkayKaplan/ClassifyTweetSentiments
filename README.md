f13c86652d39ecdd4b92f109188343f8fb77bc38
ImBerkayKaplan <kaplanberky@gmail.com>
2020-05-05 12:57:19 -0400

    Copied the project to the repository

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # RemoveFrequentWordsFromTweets
+Several techniques exist to classify certain sentences based on specific cues. In this project, Naive Bayes will be used to classify example tweets as negative or positive sentiment. The positive sentiment tweets are indicated with a label of 1, while the negative sentiment tweets are represented with -1 in the ```tweets.csv``` file. In addition, most frequent words from the tweets will be removed to demonstrate the negative effects on the accuracy rate.
+
+## Setup
+
+Certain modules must be installed before execution. The list of modules are: 
+* numpy
+* pandas
+* sklearn
+
+To install numpy, use the ```pip install numpy``` command.
+To install pandas, use the ```pip install pandas``` command.
+To install sklearn, use the ```pip install scikit-learn``` command.
+
+## Execution
+
+To run, simply enter ```python main.py``` into the command window while you're in the project directory and the ```tweets.csv``` is present in the same folder where ```main.py``` exists. The results will be printed out to the command window.
---
+++ main.py
@@ -0,0 +1,121 @@
+import pandas as pd, numpy as np
+from operator import add, truediv
+from sklearn.model_selection import train_test_split
+
+Tweets=pd.read_csv('tweets.csv')
+SEED = 42
+
+# Create the word dictionary
+N = Tweets.shape[0]
+WordDict = {}
+idCounter = 0
+AllWords = []
+for i in range(N):
+    Words = Tweets.iloc[i,1].split(" ")
+    for Word in Words:
+        if Word not in WordDict:
+            WordDict[Word] = idCounter
+            AllWords.append(Word)
+            idCounter += 1
+FeatureVectors = np.zeros((N, idCounter),dtype='float')
+
+# Encode the tweets into featureVectors and forget the frequencies
+for i in range(N):
+    Words = Tweets.iloc[i,1].split(" ")
+    for Word in Words:
+        FeatureVectors[i, WordDict[Word]]  = 1
+Labels = np.array(Tweets.iloc[:,2])
+
+# Calculate the frequence probabilities
+xTrain, xTest, yTrain, yTest = train_test_split(FeatureVectors, Labels, test_size = 0.2, random_state = SEED)
+probWordGivenPositive = xTrain[yTrain == 1,].mean(axis = 0)
+probWordGivenNegative = xTrain[yTrain == -1,].mean(axis = 0)
+priorPositive = len(yTrain[yTrain == 1])/len(yTrain)
+priorNegative = len(yTrain[yTrain == -1])/len(yTrain)
+
+# Compute the log priors to avoid underflowing
+logProbWordPresentGivenPositive = np.log(probWordGivenPositive + 0.0001)
+logProbWordAbsentGivenPositive = np.log(1 - probWordGivenPositive + 0.0001)
+logProbWordPresentGivenNegative = np.log(probWordGivenNegative + 0.0001)
+logProbWordAbsentGivenNegative = np.log(1 - probWordGivenNegative + 0.0001)
+logPriorPositive = np.log(priorPositive + 0.0001)
+logPriorNegative = np.log(priorNegative + 0.0001)
+
+def classifyNBIntegrated(words,logProbWordPresentGivenPositive, logProbWordAbsentGivenPositive,logProbWordPresentGivenNegative, logProbWordAbsentGivenNegative, logPriorPositive, logPriorNegative):
+    positive_present = logPriorPositive+np.sum(logProbWordPresentGivenPositive[words==1])
+    negative_present = logPriorNegative+np.sum(logProbWordPresentGivenNegative[words==1])
+    positive_present += np.sum(logProbWordAbsentGivenPositive[words==0])
+    negative_present += np.sum(logProbWordAbsentGivenNegative[words==0])
+    Result = 0
+    if  positive_present > negative_present:
+        Result = 1
+    else:
+        Result = -1
+    return Result
+
+def classifyNBPresent(words,logProbWordPresentGivenPositive,logProbWordPresentGivenNegative,logPriorPositive, logPriorNegative):
+    positive_present = logPriorPositive+np.sum(logProbWordPresentGivenPositive[words==1])
+    negative_present = logPriorNegative+np.sum(logProbWordPresentGivenNegative[words==1])
+    Result = 0
+    if  positive_present > negative_present:
+        Result = 1
+    else:
+        Result = -1
+    return Result
+
+def ReportAccuracy(xTest, yTest, logProbWordPresentGivenPositive, logProbWordAbsentGivenPositive, logProbWordPresentGivenNegative, logProbWordAbsentGivenNegative, logPriorPositive, logPriorNegative, Title):
+    PresentAccuracy = 0
+    PresentAbsentAccuracy = 0
+    N = len(xTest)
+    for i in range(N):
+        if classifyNBPresent(xTest[i],logProbWordPresentGivenPositive,logProbWordPresentGivenNegative,logPriorPositive, logPriorNegative) == yTest[i]:
+            PresentAccuracy += 1
+        if classifyNBIntegrated(xTest[i],logProbWordPresentGivenPositive, logProbWordAbsentGivenPositive,logProbWordPresentGivenNegative, logProbWordAbsentGivenNegative, logPriorPositive, logPriorNegative) == yTest[i]:
+            PresentAbsentAccuracy += 1
+    print(Title)
+    print("The accuracy of the system without the absent words: %.2f" % (PresentAccuracy/len(yTest)))
+    print("The accuracy of the system with the absent words incorporated: %.2f\n" % (PresentAbsentAccuracy/len(yTest)))
+
+def PerformExperiment(List, Labels, Tweets, Title):
+    N = Tweets.shape[0]
+    WordDict = {}
+    idCounter = 0
+    for Row in List:
+        Word = Row[1]
+        if Word not in WordDict:
+            WordDict[Word] = idCounter
+            AllWords.append(Word)
+            idCounter += 1
+    FeatureVectors = np.zeros((N, idCounter),dtype='float')
+    for i in range(N):
+        Words = Tweets.iloc[i,1].split(" ")
+        for Word in Words:
+            if Word in WordDict:
+                FeatureVectors[i, WordDict[Word]]  = 1
+    # Split the data into training and test sets
+    xTrain, xTest, yTrain, yTest = train_test_split(FeatureVectors, Labels, test_size = 0.2, random_state = SEED)
+    probWordGivenPositive = xTrain[yTrain == 1,].mean(axis = 0)
+    probWordGivenNegative = xTrain[yTrain == -1,].mean(axis = 0)
+    priorPositive = len(yTrain[yTrain == 1])/len(yTrain)
+    priorNegative = len(yTrain[yTrain == -1])/len(yTrain)
+
+    # Compute the log priors to avoid underflowing
+    logProbWordPresentGivenPositive = np.log(probWordGivenPositive + 0.0001)
+    logProbWordAbsentGivenPositive = np.log(1 - probWordGivenPositive + 0.0001)
+    logProbWordPresentGivenNegative = np.log(probWordGivenNegative + 0.0001)
+    logProbWordAbsentGivenNegative = np.log(1 - probWordGivenNegative + 0.0001)
+    logPriorPositive = np.log(priorPositive + 0.0001)
+    logPriorNegative = np.log(priorNegative + 0.0001)
+    ReportAccuracy(xTest, yTest, logProbWordPresentGivenPositive, logProbWordAbsentGivenPositive, logProbWordPresentGivenNegative, logProbWordAbsentGivenNegative, logPriorPositive, logPriorNegative, Title)
+
+ReportAccuracy(xTest, yTest, logProbWordPresentGivenPositive, logProbWordAbsentGivenPositive, logProbWordPresentGivenNegative, logProbWordAbsentGivenNegative, logPriorPositive, logPriorNegative, "Accuracy without any stop words")
+
+print("-------------------------- Part 2 --------------------------------\n")
+FrequencyFeatureVectors = np.zeros(len(FeatureVectors[0]))
+for i in FeatureVectors:
+    FrequencyFeatureVectors = np.add(FrequencyFeatureVectors,i)
+SortedList = sorted(list(map(list, zip(FrequencyFeatureVectors, AllWords))),key = lambda x: x[0], reverse = True)
+PerformExperiment(SortedList[25:], Labels, Tweets,"Accuracy when top 25 removed")
+PerformExperiment(SortedList[50:], Labels, Tweets,"Accuracy when top 50 removed")
+PerformExperiment(SortedList[100:], Labels, Tweets,"Accuracy when top 100 removed")
+PerformExperiment(SortedList[200:], Labels, Tweets,"Accuracy when top 200 removed")
